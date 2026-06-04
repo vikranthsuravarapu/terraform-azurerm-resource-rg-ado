3468c59d286fe48f3f63f90332ddb7d739788844
vikranth suravarapu <v-viksur@outlook.com>
2026-06-04 06:18:36 +0200

    added default variables

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
 variable "resource_group_name" {
   description = "Name of the Resource Group to be created"
   type        = string
+  default = "viktest"
 
 }
 
 variable "location" {
   description = "Location of the Resource Group to be created"
   type        = string
+  default = "southindia"
 }
